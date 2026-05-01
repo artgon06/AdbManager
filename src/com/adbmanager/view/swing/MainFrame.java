3fbda200cae37fa6401d37a5c0eb2bc024834876
Lucas Rubio <65956956+soywig@users.noreply.github.com>
2026-05-01 23:13:37 +0200

    adjust minimum window dimensions

--- src/com/adbmanager/view/swing/MainFrame.java
+++ src/com/adbmanager/view/swing/MainFrame.java
@@ -1285,8 +1285,8 @@ public class MainFrame extends JFrame {
 
     private void buildFrame() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setMinimumSize(new Dimension(1240, 820));
-        setSize(new Dimension(1440, 900));
+        setMinimumSize(new Dimension(900, 700));
+        setSize(new Dimension(1200, 800));
         setLocationRelativeTo(null);
         getContentPane().setLayout(new BorderLayout());
 
